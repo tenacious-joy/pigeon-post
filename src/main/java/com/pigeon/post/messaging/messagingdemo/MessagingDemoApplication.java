c5b21856a6a501dd42336003d602278334ea83d4
aj <Aswini.J@mrcooper.com>
2018-09-04 01:24:58 +0530

    pigeon-post project setup

---
+++ src/main/java/com/pigeon/post/messaging/messagingdemo/MessagingDemoApplication.java
@@ -0,0 +1,12 @@
+package com.pigeon.post.messaging.messagingdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagingDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagingDemoApplication.class, args);
+	}
+}
